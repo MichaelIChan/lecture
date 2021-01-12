8bdb1b6bc9b67cdce7592ef099894d15b4c81c6e
chengzh <loveandloss@hotmail.com>
2021-01-12 22:51:10 +0800

    add merge sort for given compare function

--- algorithms-part1/week_3/Mergesort/code/MergeSort.h
+++ algorithms-part1/week_3/Mergesort/code/MergeSort.h
@@ -31,6 +31,27 @@ void __merge(RandomAccessIterator first, RandomAccessIterator last)
     }
 }
 
+template <class RandomAccessIterator, class Compare>
+void __merge(RandomAccessIterator first, RandomAccessIterator last, Compare comp)
+{
+    if (first == last - 1) return;
+    typedef typename std::iterator_traits<RandomAccessIterator>::value_type T;
+    std::vector<T> aux;
+    for (RandomAccessIterator iter = first; iter != last; ++iter) {
+        aux.push_back(*iter);
+    }
+
+    RandomAccessIterator mid = aux.begin() + (aux.end() - 1  - aux.begin()) / 2;
+    RandomAccessIterator first1 = aux.begin(), last1 = mid + 1;
+    RandomAccessIterator first2 = mid + 1, last2 = aux.end();
+    for (RandomAccessIterator iter = first; iter != last; ++iter) {
+        if (std::distance(last1, first1) >= 0)      *iter = *first2++;
+        else if (std::distance(last2, first2) >= 0) *iter = *first1++;
+        else if (__less(*first2, *first1, comp))    *iter = *first2++;
+        else                                        *iter = *first1++;
+    }
+}
+
 /* 
  * merge_sort() assistent function
 **/
@@ -44,6 +65,16 @@ void __sort(RandomAccessIterator first, RandomAccessIterator last)
     __merge(first, last);
 }
 
+template <class RandomAccessIterator, class Compare>
+void __sort(RandomAccessIterator first, RandomAccessIterator last, Compare comp)
+{
+    if (std::distance(first, last) == 1) return;
+    RandomAccessIterator mid = first + (last - 1 - first) / 2;
+    __sort(first, mid + 1, comp);
+    __sort(mid + 1, last, comp);
+    __merge(first, last, comp);
+}
+
 /* 
  * merge_sort() interface
 **/
@@ -51,6 +82,12 @@ template <class RandomAccessIterator>
 void merge_sort(RandomAccessIterator first, RandomAccessIterator last)
 {
     __sort(first, last);
+}
+
+template <class RandomAccessIterator, class Compare>
+void merge_sort(RandomAccessIterator first, RandomAccessIterator last, Compare comp)
+{
+    __sort(first, last, comp);
 }
 
 #endif /* __MERGE_SORT_H */
