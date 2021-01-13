4f2cba1f416b8eb807e125c4c5863d8ab84bd251
chengzh <loveandloss@hotmail.com>
2021-01-13 20:25:31 +0800

    add merge sort from bottom to up

--- algorithms-part1/week_3/Mergesort/code/MergeSort.h
+++ algorithms-part1/week_3/Mergesort/code/MergeSort.h
@@ -11,18 +11,24 @@
  * merge [first, mid] and [mid + 1, last]
 **/
 template <class RandomAccessIterator>
-void __merge(RandomAccessIterator first, RandomAccessIterator last)
+void __merge(RandomAccessIterator first, RandomAccessIterator mid, RandomAccessIterator last)
 {
     if (first == last - 1) return;
     typedef typename std::iterator_traits<RandomAccessIterator>::value_type T;
-    std::vector<T> aux;
-    for (RandomAccessIterator iter = first; iter != last; ++iter) {
-        aux.push_back(*iter);
+    std::vector<T> aux = std::vector<T>(last - first);
+
+    RandomAccessIterator auxMid = aux.begin();
+    int i = 0;
+    for (RandomAccessIterator iter = first; iter != mid; ++iter) {
+        aux[i++] = *iter;
+        auxMid++;
+    }
+    for (RandomAccessIterator iter = mid; iter != last; ++iter) {
+        aux[i++] = *iter;
     }
 
-    RandomAccessIterator mid = aux.begin() + (aux.end() - 1  - aux.begin()) / 2;
-    RandomAccessIterator first1 = aux.begin(), last1 = mid + 1;
-    RandomAccessIterator first2 = mid + 1, last2 = aux.end();
+    RandomAccessIterator first1 = aux.begin(), last1 = auxMid;
+    RandomAccessIterator first2 = auxMid, last2 = aux.end();
     for (RandomAccessIterator iter = first; iter != last; ++iter) {
         if (std::distance(last1, first1) >= 0)      *iter = *first2++;
         else if (std::distance(last2, first2) >= 0) *iter = *first1++;
@@ -32,18 +38,24 @@ void __merge(RandomAccessIterator first, RandomAccessIterator last)
 }
 
 template <class RandomAccessIterator, class Compare>
-void __merge(RandomAccessIterator first, RandomAccessIterator last, Compare comp)
+void __merge(RandomAccessIterator first, RandomAccessIterator mid, RandomAccessIterator last, Compare comp)
 {
     if (first == last - 1) return;
     typedef typename std::iterator_traits<RandomAccessIterator>::value_type T;
-    std::vector<T> aux;
-    for (RandomAccessIterator iter = first; iter != last; ++iter) {
-        aux.push_back(*iter);
+    std::vector<T> aux = std::vector<T>(last - first);
+
+    RandomAccessIterator auxMid = aux.begin();
+    int i = 0;
+    for (RandomAccessIterator iter = first; iter != mid; ++iter) {
+        aux[i++] = *iter;
+        auxMid++;
+    }
+    for (RandomAccessIterator iter = mid; iter != last; ++iter) {
+        aux[i++] = *iter;
     }
 
-    RandomAccessIterator mid = aux.begin() + (aux.end() - 1  - aux.begin()) / 2;
-    RandomAccessIterator first1 = aux.begin(), last1 = mid + 1;
-    RandomAccessIterator first2 = mid + 1, last2 = aux.end();
+    RandomAccessIterator first1 = aux.begin(), last1 = auxMid;
+    RandomAccessIterator first2 = auxMid, last2 = aux.end();
     for (RandomAccessIterator iter = first; iter != last; ++iter) {
         if (std::distance(last1, first1) >= 0)      *iter = *first2++;
         else if (std::distance(last2, first2) >= 0) *iter = *first1++;
@@ -62,7 +74,7 @@ void __sort(RandomAccessIterator first, RandomAccessIterator last)
     RandomAccessIterator mid = first + (last - 1 - first) / 2;
     __sort(first, mid + 1);
     __sort(mid + 1, last);
-    __merge(first, last);
+    __merge(first, mid + 1, last);
 }
 
 template <class RandomAccessIterator, class Compare>
@@ -72,7 +84,7 @@ void __sort(RandomAccessIterator first, RandomAccessIterator last, Compare comp)
     RandomAccessIterator mid = first + (last - 1 - first) / 2;
     __sort(first, mid + 1, comp);
     __sort(mid + 1, last, comp);
-    __merge(first, last, comp);
+    __merge(first, mid + 1, last, comp);
 }
 
 /* 
@@ -88,6 +100,33 @@ template <class RandomAccessIterator, class Compare>
 void merge_sort(RandomAccessIterator first, RandomAccessIterator last, Compare comp)
 {
     __sort(first, last, comp);
+}
+
+/* 
+ * merge_sortBU() interface
+**/
+template <class RandomAccessIterator>
+void merge_sortBU(RandomAccessIterator first, RandomAccessIterator last)
+{
+    for (int sz = 1; sz < std::distance(first, last); sz = sz + sz) {
+        std::cout << "sz = " << sz << std::endl;
+        for (RandomAccessIterator left = first; std::distance(left, last - sz) > 0; left += sz + sz) {
+            RandomAccessIterator right = std::distance(left + sz + sz, last) > 0 ? left + sz + sz : last;
+            __merge(left, left + sz, right);
+        }
+    }
+}
+
+template <class RandomAccessIterator, class Compare>
+void merge_sortBU(RandomAccessIterator first, RandomAccessIterator last, Compare comp)
+{
+    for (int sz = 1; sz < std::distance(first, last); sz = sz + sz) {
+        std::cout << "sz = " << sz << std::endl;
+        for (RandomAccessIterator left = first; std::distance(left, last - sz) > 0; left += sz + sz) {
+            RandomAccessIterator right = std::distance(left + sz + sz, last) > 0 ? left + sz + sz : last;
+            __merge(left, left + sz, right, comp);
+        }
+    }
 }
 
 #endif /* __MERGE_SORT_H */
